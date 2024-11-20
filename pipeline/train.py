b3f414a7205166ee7b8d9c35d87579a4f0d0fc76
ioSoyPato <patrikwars@gmail.com>
2024-11-19 22:03:38 -0600

    added clean data and train.py

---
+++ pipeline/train.py
@@ -0,0 +1,52 @@
+import prefect 
+from prefect import flow, task
+import pandas as pd
+import json
+
+
+
+@task(name="readData")
+async def readData(path:str)-> json:
+    with open(path, "r") as f:
+        data = json.load(f)
+
+    return data
+
+@task(name="normalize")
+async def normalize(Json:json) -> pd.DataFrame:
+    return pd.json_normalize(Json)
+
+
+@task(name="preprocess")
+async def preprocessData(df:pd.DataFrame) -> None:
+    df = df[["_source.complaint_what_happened","_source.product","_source.sub_product"]]
+
+    rename = {"_source.complaint_what_happened":"complaint_what_happened",
+    "_source.product":"category",
+    "_source.sub_product":"sub_product"}
+
+    df = df.rename(rename, axis=1)
+
+    df["ticket_classification"] = df["category"] + " + " + df["sub_product"]
+    df = df.drop(columns=["category","sub_product"],axis=1)
+    df["complaint_what_happened"] = df["complaint_what_happened"].replace("",pd.NA)
+
+    df.to_csv("../data/preprocessed_data/preprocessed.csv",index=False)
+
+    return None
+
+
+@flow(name="mainFlow")
+async def mainFlow(path:str)->None:
+    # data almacena un tipo de dato json que contiene la informacion actualizada del rawData
+    data = readData(path)
+    # df almacena un tipo de dato DataFrame que contiene el json normalizado
+    df = normalize(data)
+    # preprocessData es una funcion sin tipo de dato de retorno que actualiza el csv contenido en data/proprocessed_data/ con los datos limpios y listos para procesarse
+    preprocessData(df)
+    
+
+
+
+
+mainFlow("../data/raw_data/tickets_classification_eng.json")
