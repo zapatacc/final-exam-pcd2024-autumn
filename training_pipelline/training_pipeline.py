62bb0da823b3ba5baf31dc646db2e394fd7aeb73
JuanPab2009 <pablo.blanco@iteso.mx>
2024-11-22 18:58:26 -0600

    training_pipeline.py comentado

--- training_pipelline/training_pipeline.py
+++ training_pipelline/training_pipeline.py
@@ -1,45 +1,45 @@
 import pathlib
-
+import dagshub
 from prefect import task, flow
 import pandas as pd
 import numpy as np
 import os
 import json
 import pickle
-
-# Librerías para mlflow
 import mlflow
 import mlflow.sklearn
 from mlflow.tracking import MlflowClient
-
-# Librerías para preprocesamiento y modelado
 from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import LogisticRegression
 from sklearn.svm import SVC
-from sklearn.metrics import accuracy_score, classification_report
-from sklearn.pipeline import Pipeline
+from sklearn.metrics import accuracy_score
 from sklearn.preprocessing import LabelEncoder
-
-# Librerías para preprocesamiento de texto
 import nltk
 import re
 import contractions
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
 
-# Ignorar advertencias
+nltk.download('stopwords')
+nltk.download('wordnet')
+nltk.download('punkt')
+nltk.download('punkt_tab')
+
 import warnings
+
 warnings.filterwarnings('ignore')
 
-import dagshub
-
-import dagshub
+# Iniciamos dagshub
 dagshub.init(repo_owner='zapatacc', repo_name='final-exam-pcd2024-autumn', mlflow=True)
 
+
+# Función para expandir contracciones
 def expand_contractions(text):
     return contractions.fix(text)
 
+
+# Función para limpiar el texto
 def clean_text(text):
     text = expand_contractions(text)
     text = text.lower()
@@ -47,21 +47,20 @@ def clean_text(text):
     text = re.sub(r'\s+', ' ', text).strip()
     return text
 
+
+# Función para preprocesar el texto
 def preprocess_text(text):
-    # Limpiar el texto
     text = clean_text(text)
-    # Tokenizar
     words = nltk.word_tokenize(text)
-    # Eliminar stopwords
     stop_words = set(stopwords.words('english'))
     words = [word for word in words if word not in stop_words]
-    # Lematizar
     lemmatizer = WordNetLemmatizer()
     words = [lemmatizer.lemmatize(word) for word in words]
-    # Unir palabras
     text = ' '.join(words)
     return text
 
+
+# Función para filtrar categorías con pocas ocurrencias
 def filter_categories(df, min_frequency=80):
     value_counts = df['ticket_classification'].value_counts()
     categories_to_keep = value_counts[value_counts >= min_frequency].index
@@ -69,6 +68,7 @@ def filter_categories(df, min_frequency=80):
     return filtered_df
 
 
+# Tarea para cargar y preparar los datos
 @task(task_run_name='Data Cleaning and loading')
 def load_and_prepare_data():
     file_path = '../data/raw_data/tickets_classification_eng.json'
@@ -79,10 +79,10 @@ def load_and_prepare_data():
         with open(file_path, "r") as file:
             datos = json.load(file)
 
-    # Normalizar datos y crear DataFrame
+    # Convertimos los datos JSON a un DataFrame de pandas
     df = pd.json_normalize(datos)
 
-    # Seleccionar y renombrar columnas
+    # Seleccionamos y renombramos las columnas
     df_clean = df[['_source.complaint_what_happened', '_source.product', '_source.sub_product']]
     df_clean.rename(columns={
         '_source.complaint_what_happened': 'complaint_what_happened',
@@ -90,48 +90,55 @@ def load_and_prepare_data():
         '_source.sub_product': 'sub_product'
     }, inplace=True)
 
-    # Crear nueva columna 'ticket_classification'
+    # Creamos una nueva columna combinando 'category' y 'sub_product'
     df_clean['ticket_classification'] = df_clean['category'] + ' + ' + df_clean['sub_product']
 
-    # Eliminar columnas redundantes
+    # Eliminamos las columnas que ya no necesitamos
     df_clean.drop(['category', 'sub_product'], axis=1, inplace=True)
 
-    # Reemplazar campos vacíos en 'complaint_what_happened' por NaN
+    # Reemplazamos cadenas vacías por NaN
     df_clean['complaint_what_happened'].replace('', pd.NA, inplace=True)
 
-    # Eliminar filas con datos faltantes
+    # Eliminamos filas con datos faltantes
     df_clean.dropna(subset=['complaint_what_happened', 'ticket_classification'], inplace=True)
 
-    # Reiniciar índice
+    # Reiniciamos el índice del DataFrame
     df_clean.reset_index(drop=True, inplace=True)
 
     return df_clean
 
+
+# Tarea para preprocesar los datos
 @task(task_run_name='Data Preprocessing')
 def preprocess_data(df):
     df['complaint_what_happened'] = df['complaint_what_happened'].apply(preprocess_text)
     return df
 
-@task(task_run_name='Data Filrering')
+
+# Tarea para filtrar los datos según la frecuencia de las categorías
+@task(task_run_name='Data Filtering')
 def filter_data(df, min_frequency=80):
     df_filtered = filter_categories(df, min_frequency)
     return df_filtered
 
+
+# Tarea para dividir el train test
 @task(task_run_name='Split Data')
 def split_data(df):
     X = df['complaint_what_happened']
     y = df['ticket_classification']
+
     label_encoder = LabelEncoder()
 
-    # Ajustar el LabelEncoder con las etiquetas y transformar
+    # Codificamos las etiquetas (texto a números)
     y_encoded = label_encoder.fit_transform(y)
 
-    # Guardar el LabelEncoder ajustado
+    # Guardamos el LabelEncoder para usarlo luego en la predicción
     pathlib.Path("models").mkdir(exist_ok=True)
     with open("models/label_encoder.pkl", "wb") as file:
         pickle.dump(label_encoder, file)
 
-    # Dividir los datos
+    # Dividimos los datos en train test
     X_train, X_test, y_train, y_test = train_test_split(
         X, y_encoded, test_size=0.2, random_state=42, stratify=y_encoded
     )
@@ -139,21 +146,22 @@ def split_data(df):
     return X_train, X_test, y_train, y_test
 
 
+# Tarea para entrenar y evaluar los modelos
 @task(name='Train and Test')
 def train_and_evaluate_models(X_train, y_train, X_test, y_test):
-    # Inicializar el vectorizador TF-IDF
+    # Inicializamos el vectorizador TF-IDF
     tfidf_vectorizer = TfidfVectorizer(max_features=5000)
 
-    # Ajustar y transformar los datos de entrenamiento
+    # Ajustamos y transformamos los datos de entrenamiento
     X_train_tfidf = tfidf_vectorizer.fit_transform(X_train)
 
-    # Transformar los datos de prueba
+    # Transformamos los datos de prueba
     X_test_tfidf = tfidf_vectorizer.transform(X_test)
 
     # Diccionario para almacenar resultados
     models_results = {}
 
-    # 1. Regresión Logística Base
+    # 1. Regresión Logística Básica
     lr_baseline = LogisticRegression(max_iter=1000)
     lr_baseline.fit(X_train_tfidf, y_train)
     y_pred_lr_baseline = lr_baseline.predict(X_test_tfidf)
@@ -164,7 +172,7 @@ def train_and_evaluate_models(X_train, y_train, X_test, y_test):
         'params': lr_baseline.get_params()
     }
 
-    # 2. Regresión Logística con Hyperparameter Tuning
+    # 2. Regresión Logística con Ajuste de Hiperparámetros
     param_grid_lr = {
         'C': [0.1, 1, 10],
         'penalty': ['l1', 'l2'],
@@ -187,7 +195,7 @@ def train_and_evaluate_models(X_train, y_train, X_test, y_test):
         'params': grid_search_lr.best_params_
     }
 
-    # 3. SVC Base
+    # 3. SVC Básico
     svc_baseline = SVC()
     svc_baseline.fit(X_train_tfidf, y_train)
     y_pred_svc_baseline = svc_baseline.predict(X_test_tfidf)
@@ -198,7 +206,7 @@ def train_and_evaluate_models(X_train, y_train, X_test, y_test):
         'params': svc_baseline.get_params()
     }
 
-    # 4. SVC con Hyperparameter Tuning
+    # 4. SVC con Ajuste de Hiperparámetros
     param_grid_svc = {
         'C': [0.1, 1, 10],
         'kernel': ['linear', 'rbf'],
@@ -220,29 +228,30 @@ def train_and_evaluate_models(X_train, y_train, X_test, y_test):
         'accuracy': accuracy_svc_tuned,
         'params': grid_search_svc.best_params_
     }
-    # Guardar el vectorizador
+    # Guardamos el vectorizador
     with open('models/tfidf_vectorizer.pkl', 'wb') as f:
         pickle.dump(tfidf_vectorizer, f)
 
     return models_results
 
 
-
 EXPERIMENT_NAME = 'blanco-all-models-prefect'
 
+
+# Tarea para registrar los modelos en MLflow
 @task(name="log")
 def log_models(models_results):
-    # Configurar mlflow
+    # Configuramos MLflow
     mlflow.set_tracking_uri('https://dagshub.com/zapatacc/final-exam-pcd2024-autumn.mlflow')
     client = MlflowClient()
 
-    # Establecer el experimento
+    # Establecemos el experimento
     mlflow.set_experiment(EXPERIMENT_NAME)
 
     # Lista para almacenar información de los runs
     run_infos = []
 
-    # Registrar modelos y logs
+    # Registramos cada modelo y sus métricas
     for model_name, result in models_results.items():
         with mlflow.start_run(run_name=model_name):
             mlflow.log_params(result['params'])
@@ -253,9 +262,10 @@ def log_models(models_results):
             )
             pathlib.Path("models").mkdir(exist_ok=True)
 
+            # Guardamos el LabelEncoder y el vectorizador como artefactos
             mlflow.log_artifact("models/label_encoder.pkl", artifact_path="LabelEncoder")
             mlflow.log_artifact("models/tfidf_vectorizer.pkl", artifact_path="Vectorizer")
-            # Obtener run_id y almacenar información
+            # Obtenemos el run_id y guardamos la info
             run_id = mlflow.active_run().info.run_id
             run_infos.append({
                 'model_name': model_name,
@@ -263,14 +273,13 @@ def log_models(models_results):
                 'accuracy': result['accuracy']
             })
 
-    # Después de registrar todos los modelos, seleccionar Champion y Challenger
-    # Ordenar los runs por accuracy
+    # Ordenamos los runs por accuracy para encontrar nuestro champion
     sorted_runs = sorted(run_infos, key=lambda x: x['accuracy'], reverse=True)
 
-    # Registrar Champion y Challenger en el Model Registry
+    # Nombre del modelo
     registered_model_name = 'blanco-models-prefect'
 
-    # Modelo Champion
+    # Registramos el Champion
     champion_run = sorted_runs[0]
     champion_model_uri = f"runs:/{champion_run['run_id']}/model"
 
@@ -285,7 +294,7 @@ def log_models(models_results):
         version=champion_model_version.version
     )
 
-    # Modelo Challenger (si existe más de un modelo)
+    # Si hay más de un modelo, registramos de los que quedan el siguiente mejor (Challenger)
     if len(sorted_runs) > 1:
         challenger_run = sorted_runs[1]
         challenger_model_uri = f"runs:/{challenger_run['run_id']}/model"
@@ -301,33 +310,26 @@ def log_models(models_results):
             version=challenger_model_version.version
         )
 
-    # Imprimir resultados
-    print(f"Champion Model: {champion_run['model_name']}, Run ID: {champion_run['run_id']}, Version: {champion_model_version.version}")
+    # Imprimimos los resultados
+    print(
+        f"Champion Model: {champion_run['model_name']}, Run ID: {champion_run['run_id']}, Version: {champion_model_version.version}")
     if len(sorted_runs) > 1:
-        print(f"Challenger Model: {challenger_run['model_name']}, Run ID: {challenger_run['run_id']}, Version: {challenger_model_version.version}")
+        print(
+            f"Challenger Model: {challenger_run['model_name']}, Run ID: {challenger_run['run_id']}, Version: {challenger_model_version.version}")
 
 
-
-
+# Definimos el flujo principal
 @flow()
 def main():
-    # Cargar y preparar datos en bruto
     df_clean = load_and_prepare_data()
-
-    # Preprocesar datos
     df_preprocessed = preprocess_data(df_clean)
-
-    # Filtrar categorías por frecuencia
     df_filtered = filter_data(df_preprocessed, min_frequency=80)
-
-    # Dividir datos
     X_train, X_test, y_train, y_test = split_data(df_filtered)
-
-    # Entrenar y evaluar modelos
     models_results = train_and_evaluate_models(X_train, y_train, X_test, y_test)
 
-    # Registrar modelos
     log_models(models_results)
 
+
+# Ejecutamos el flujo principal
 if __name__ == '__main__':
     main()
